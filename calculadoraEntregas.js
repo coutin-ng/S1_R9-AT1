cffc563157e6259a0cfbabe76a40c5b627b096f8
unknown <coutinhnicolasgabriel@gmail.com>
2025-06-17 15:03:23 -0300

    Algoritmo quase pronto, falta só criar o arquivo de texto

--- calculadoraEntregas.js
+++ calculadoraEntregas.js
@@ -3,26 +3,65 @@ const prompSync = require("prompt-sync")();
 
 let nomes = [];
 let enderecos = [];
-let continuar;
+let distancias = [];
+let valorKm = [];
+let tipoEntregas = [];
+let continuar = 1;
+let custoEntregas = [];
+let i;
 
-do {
+for (let i = 0; continuar == 1; i++) {
 
     do {
 
-        nomes = prompSync(`Insira o nome do cliente: `);
+        nomes[i] = prompSync(`Insira o nome do cliente: `);
 
-    } while (!(isNaN(nomes)));
+    } while (!(isNaN(nomes[i])));
 
     do {
 
-        enderecos = prompSync(`Insira o endereço do cliente: `);
+        enderecos[i] = prompSync(`Insira o endereço do cliente: `);
 
-    } while (!(isNaN(enderecos)));
+    } while (!(isNaN(enderecos[i])));
+
+    do {
+
+        distancias[i] = prompSync(`Insira a distância da entrega (em quilômetros): `);
+
+    } while (isNaN(distancias[i]) || distancias == 0);
+
+    do {
+
+        valorKm[i] = prompSync(`Insira o valor cobrado por quilômetro: `);
+
+    } while (isNaN(valorKm[i]) || valorKm == 0);
+
+    do {
+
+        tipoEntregas[i] = prompSync(`Entrega normal ou urgente? Digite 1 para urgente: `);
+
+    } while (isNaN(tipoEntregas[i]));
+
+    if (tipoEntregas[i] == 1) {
+
+        custoEntregas[i] = (`Valor da entrega: R$${((distancias[i] * valorKm[i]) * 1.2 .toFixed(2))}`);
+        tipoEntregas[i] = "Urgente";
+
+    } else {
+
+        custoEntregas[i] = (`Valor da entrega: R$${(distancias[i] * valorKm[i]) .toFixed(2)}`);
+        tipoEntregas[i] = "Normal";
+
+    }
+    console.log(`\n ${nomes}\n ${enderecos} \n ${tipoEntregas} \n ${custoEntregas}`);
 
     do {
 
         continuar = prompSync(`Você deseja continuar? Digite 1 para continuar: `);
-        
-    } while (isNaN(continuar));
 
-} while (continuar == 1);
+    } while (isNaN(continuar));
+   
+    console.log(`\n Total de entregas: ${i+1}`);
+}
+
+
